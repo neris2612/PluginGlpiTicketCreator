9b1cf578e4312953decca525c84df75168e29eb6
neris2612 <56694425+neris2612@users.noreply.github.com>
2025-07-30 16:14:41 -0300

    Update hook.php

--- hook.php
+++ hook.php
@@ -1,8 +1,26 @@
 <?php
-function plugin_importcsvtickets_install() {
+
+function plugin_init_importcsvtickets() {
+    global $PLUGIN_HOOKS;
+
+    $PLUGIN_HOOKS['menu_toadd']['central'][] = 'PluginImportcsvticketsImportcsvtickets';
+}
+
+function plugin_version_importcsvtickets() {
+    return [
+        'name'           => 'Importador CSV de Chamados',
+        'version'        => '1.0.0',
+        'author'         => 'Neris',
+        'license'        => 'GPLv2+',
+        'homepage'       => '',
+        'minGlpiVersion' => '10.0.0'
+    ];
+}
+
+function plugin_importcsvtickets_check_prerequisites() {
     return true;
 }
 
-function plugin_importcsvtickets_uninstall() {
+function plugin_importcsvtickets_check_config() {
     return true;
 }
