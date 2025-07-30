bc30d4005c45422a052942428df6b08ba1499c00
neris2612 <56694425+neris2612@users.noreply.github.com>
2025-07-30 16:15:37 -0300

    Update importcsvtickets.class.php

--- inc/importcsvtickets.class.php
+++ inc/importcsvtickets.class.php
@@ -1,23 +1,9 @@
 <?php
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
 class PluginImportcsvticketsImportcsvtickets extends CommonDBTM {
 
-    public static function getTypeName($nb = 0) {
-        return __('Import CSV Tickets', 'importcsvtickets');
-    }
-
-    function canCreate() {
-        return Session::haveRight("ticket", CREATE);
-    }
-
-    function canView() {
-        return Session::haveRight("ticket", READ);
-    }
-
-    function canUpdate() {
-        return Session::haveRight("ticket", UPDATE);
-    }
-
-    function canDelete() {
-        return Session::haveRight("ticket", DELETE);
-    }
 }
