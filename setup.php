80c04f6e054a1ebbe251e52bd9cded49cd33d288
neris2612 <56694425+neris2612@users.noreply.github.com>
2025-07-30 16:15:04 -0300

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,26 +1,19 @@
 <?php
-function plugin_init_importcsvtickets() {
-    global $PLUGIN_HOOKS;
-
-    $PLUGIN_HOOKS['csrf_compliant']['importcsvtickets'] = true;
-    $PLUGIN_HOOKS['menu_toadd']['importcsvtickets'] = ['tools' => 'importcsvtickets'];
-}
 
 function plugin_version_importcsvtickets() {
-    return [
-        'name'           => 'Import CSV Tickets',
-        'version'        => '1.0.0',
-        'author'         => 'Neris',
-        'license'        => 'GPLv3+',
-        'homepage'       => 'https://seusite.com',
-        'minGlpiVersion' => '10.0.0'
-    ];
+   return [
+      'name'           => "Importador CSV de Chamados",
+      'version'        => '1.0.0',
+      'author'         => 'Neris',
+      'license'        => 'GPLv2+',
+      'minGlpiVersion' => '10.0.0'
+   ];
 }
 
-function plugin_importcsvtickets_check_prerequisites() {
-    return true;
+function plugin_importcsvtickets_install() {
+   return true;
 }
 
-function plugin_importcsvtickets_check_config() {
-    return true;
+function plugin_importcsvtickets_uninstall() {
+   return true;
 }
